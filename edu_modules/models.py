f083fa3d8a272d56bf0479b93faab065fd126dfb
Максим <javoprav@gmail.com>
2023-08-28 18:37:29 +0300

    Initial commit

---
+++ edu_modules/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Module(models.Model):
+    number = models.ImageField(verbose_name='порядковый номер')
+    name = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='картинка', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='обновлен', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Образовательный модуль'
+        verbose_name_plural = 'Образовательные модули'
+
+    def __str__(self):
+        return f'{self.name}'
