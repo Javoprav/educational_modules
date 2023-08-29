d13c6e69137a7685b8f1df559037b88b2d2fe8f6
Максим <javoprav@gmail.com>
2023-08-29 22:55:53 +0300

    add test and fixtures

--- edu_modules/tests.py
+++ edu_modules/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User, UserRoles
 
-# Create your tests here.
+
+class ModuleTestCase(APITestCase):
+    """Тесты модели Module"""
+    def setUp(self) -> None:
+        """Подготовка данных перед тестом"""
+        self.user = User.objects.create(
+            email='user@user.com',
+            is_staff=False,
+            is_superuser=False,
+            is_active=True,
+            role=UserRoles.MEMBER,
+        )
+        self.user.set_password('123')
+        self.user.save()
+        response = self.client.post('/api/token/', {"email": "user@user.com", "password": "123"})
+        self.access_token = response.json().get('access')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+        self.test_model_name = 'module_for_test'
+
+    def test_module_create(self):
+        """Тест создания модели Module"""
+        response = self.client.post('/api/edu_modules/create/', {'name': self.test_model_name})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
